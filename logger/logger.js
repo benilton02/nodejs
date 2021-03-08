3c06b6f70872d50e0ae2d54cd14426b7b0fb7bdb
Benilton <bsa.ele@uea.edu.br>
2021-03-08 00:25:15 -0500

    Studying: global events

---
+++ logger/logger.js
@@ -0,0 +1,15 @@
+const eventEmitter = require('events') // built-in
+const emitter = new eventEmitter() // criar uma instancia de 'events'
+
+
+// esperar a emissão de uma mensagem
+emitter.on('log', (message) => {
+    console.log(message)
+})
+
+// função responsável por emitir uma mensagem
+function log(message){
+    emitter.emit('log', message)
+}
+
+log('System response')
