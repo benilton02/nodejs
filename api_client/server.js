7ba256162da84e497f1c31fb5119a7b17bec5cdd
Benilton <bsa.ele@uea.edu.br>
2021-03-15 12:59:09 -0500

    feat: create client (POST)

--- api_client/server.js
+++ api_client/server.js
@@ -1,9 +1,12 @@
+const { request } = require("express")
 const express = require("express")
 const data = require("./data.json")
 
 const app = express() 
 const host = "0.0.0.0"
 const port = 3000
+
+app.use(express.json())
 
 app.get("/all", (request, response) => {
     console.log("Ok")
@@ -21,6 +24,15 @@ app.get("/find/:id", (request, response) => {
         response.status(200).send(client)
     }
     console.log(`Requested ID: ${id}`)
+})
+
+app.post("/create", (request, response) => {
+    const {id, name} = request.body
+
+    //save requested data into database
+
+    console.log({id, name})
+    response.json({id, name})
 })
 
 app.listen(port, () => {
