4ae02c3073e65aca251cdefd12da6e84a3b18147
Benilton <bsa.ele@uea.edu.br>
2021-03-18 11:45:32 -0500

    refact: update routes

--- api_client/app.js
+++ api_client/app.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const clientRouter = require("./route/client")
-
-
 const app = express()
 
-app.use("/", clientRouter)
+app.use(express.json())
+
+app.use("/client", clientRouter)
 
 module.exports = app
--- api_client/route/client.js
+++ api_client/route/client.js
@@ -1,8 +1,57 @@
 const express = require("express")
+const data = require("../config/data.json")
 const router = express.Router()
 
+
 router.get("/all", (request, response) => {
-    response.status(200).end("OK")
+    response.status(200).json(data)
+})
+
+router.get("/find/:id", (request, response) => {
+    const { id } = request.params
+    const client = data.find( cli => cli.id == id)
+    
+    if(!client){
+        response.status(204).end()
+    }
+    else{
+        response.status(200).json(client)
+    }
+    
+})
+
+router.post("/create", (request, response) => {
+    const { id, name } = request.body
+    console.log({id, name})
+    
+    //save requested data into database
+
+    response.status(201).json({id, name})
+})
+
+router.put("/update/:id", (request, response) => {
+    const {id} = request.params
+    const client = data.find( cli => cli.id == id)
+
+    if(!client){
+        response.status(204).end()
+        console.log(`Request ID: ${id}`)
+    }
+    else{
+        const {name} = request.body
+
+        // update database
+        
+        client.name = name
+        response.status(201).json(client)
+        console.log("Successfully Updated!")
+    }
+})
+
+router.delete("/delete/:id", (request, response) => {
+    const {id} = request.params
+    const newData = data.filter(client => client.id != id)
+    response.status(200).json(newData)
 })
 
 module.exports = router
